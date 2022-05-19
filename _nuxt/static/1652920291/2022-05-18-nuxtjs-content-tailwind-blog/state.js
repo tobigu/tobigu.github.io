69f1ed92d470c48412054628e3224601ed198a64
Tobias Gundry <tobygundry@gmail.com>
2022-05-19 10:35:28 +1000

    Updates

---
+++ _nuxt/static/1652920291/2022-05-18-nuxtjs-content-tailwind-blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652920291",layout:"default",error:a,serverRendered:true,routePath:"\u002F2022-05-18-nuxtjs-content-tailwind-blog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"970c9ad3"}}}}(null));
